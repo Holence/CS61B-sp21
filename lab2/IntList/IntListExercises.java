40192ff82accec7d09803fbf08bf1e3953f098cd
Holence <Holence08@gmail.com>
2024-01-21 01:45:00 +0800

    lab2

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,7 +10,7 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
@@ -46,12 +46,13 @@ public class IntListExercises {
         return max;
     }
 
-    /** Returns true if the last digit of x is equal to
-     *  the first digit of x.
+    /**
+     * Returns true if the last digit of x is equal to
+     * the first digit of x.
      */
     public static boolean firstDigitEqualsLastDigit(int x) {
         int lastDigit = x % 10;
-        while (x > 10) {
+        while (x >= 10) {
             x = x / 10;
         }
         int firstDigit = x % 10;
@@ -71,12 +72,16 @@ public class IntListExercises {
             return false;
         }
 
-        boolean currElemIsPrime = Primes.isPrime(lst.first);
-
-        if (currElemIsPrime) {
-            lst.first *= lst.first;
+        boolean changed = false;
+        IntList p = lst;
+        while (p != null) {
+            if (Primes.isPrime(p.first)) {
+                p.first *= p.first;
+                changed = true;
+            }
+            p = p.rest;
         }
 
-        return currElemIsPrime || squarePrimes(lst.rest);
+        return changed;
     }
 }
