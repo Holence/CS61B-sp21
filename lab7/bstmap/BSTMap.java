3f898b052734d3f06c99ba080b222807265a3730
Holence <Holence08@gmail.com>
2024-01-29 23:59:42 +0800

    recursive put() simple clear() iterator() & keySet()

--- lab7/bstmap/BSTMap.java
+++ lab7/bstmap/BSTMap.java
@@ -1,7 +1,9 @@
 package bstmap;
 
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.Stack;
 
 public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
@@ -14,10 +16,6 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
             this.key = key;
             this.value = value;
         }
-
-        public boolean isLeaf() {
-            return left == null && right == null;
-        }
     }
 
     private BSTNode root;
@@ -29,19 +27,8 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public void clear() {
-        clear(root);
         root = null;
         size = 0;
-    }
-
-    private void clear(BSTNode p) {
-        if (p == null) {
-            return;
-        }
-        clear(p.right);
-        p.right = null;
-        clear(p.left);
-        p.left = null;
     }
 
     @Override
@@ -85,45 +72,95 @@ public class BSTMap<K extends Comparable<K>, V> implements Map61B<K, V> {
 
     @Override
     public void put(K key, V value) {
+        root = put(root, key, value);
         size += 1;
-        if (root == null) {
-            root = new BSTNode(key, value);
-            return;
+
+        // Iterative
+        // size += 1;
+        // if (root == null) {
+        //     root = new BSTNode(key, value);
+        //     return;
+        // }
+        // BSTNode p = root;
+        // while (p != null) {
+        //     if (key.compareTo(p.key) > 0) {
+        //         if (p.right == null) {
+        //             p.right = new BSTNode(key, value);
+        //             return;
+        //         } else {
+        //             p = p.right;
+        //         }
+        //     } else if (key.compareTo(p.key) < 0) {
+        //         if (p.left == null) {
+        //             p.left = new BSTNode(key, value);
+        //             return;
+        //         } else {
+        //             p = p.left;
+        //         }
+        //     } else {
+        //         p.value = value;
+        //         return;
+        //     }
+        // }
+    }
+
+    private BSTNode put(BSTNode p, K key, V value) {
+        if (p == null) {
+            return new BSTNode(key, value);
         }
-        BSTNode p = root;
-        while (p != null) {
-            if (key.compareTo(p.key) > 0) {
-                if (p.right == null) {
-                    p.right = new BSTNode(key, value);
-                    return;
-                } else {
-                    p = p.right;
-                }
-            } else if (key.compareTo(p.key) < 0) {
-                if (p.left == null) {
-                    p.left = new BSTNode(key, value);
-                    return;
-                } else {
-                    p = p.left;
-                }
-            } else {
-                p.value = value;
-                return;
-            }
+        int cmp = key.compareTo(p.key);
+        if (cmp > 0) {
+            p.right = put(p.right, key, value);
+        } else if (cmp < 0) {
+            p.left = put(p.left, key, value);
+        } else {
+            p.value = value;
         }
+        return p;
     }
 
     @Override
     public Set<K> keySet() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'keySet'");
+        Set<K> s = new HashSet<>();
+        for (K k : this) {
+            s.add(k);
+        }
+        return s;
     }
 
     @Override
     public Iterator<K> iterator() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'iterator'");
+        return new BSTMapIterator();
     }
+
+    private class BSTMapIterator implements Iterator<K> {
+
+        private Stack<BSTNode> stack;
+
+        public BSTMapIterator() {
+            stack = new Stack<>();
+            allWayToLeft(root);
+        }
+
+        void allWayToLeft(BSTNode p) {
+            while (p != null) {
+                stack.add(p);
+                p = p.left;
+            }
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !stack.isEmpty();
+        }
+
+        @Override
+        public K next() {
+            BSTNode p = stack.pop();
+            allWayToLeft(p.right);
+            return p.key;
+        }
+    };
 
     @Override
     public V remove(K key) {
